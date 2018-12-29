660eff113c79bed0739456c3ab13cef0c3620c9e
Volong <yhlona523@gmail.com>
2018-12-30 00:57:59 +0800

    chapter 1

---
+++ chapter01/write-img-to-client.js
@@ -0,0 +1,11 @@
+
+var http = require("http");
+
+var fs = require("fs");
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {"Content-Type": "image/png"});
+	fs.createReadStream("./image.png").pipe(res);
+}).listen(3000);
+
+console.log("Server running at port 3000");
