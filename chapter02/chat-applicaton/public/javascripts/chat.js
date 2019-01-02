0073428a4c9c87a40701c2f65cb33f3bfd2e1e12
Volong <yhlona523@gmail.com>
2019-01-03 00:41:47 +0800

    chat

--- chapter02/chat-applicaton/public/javascripts/chat.js
+++ chapter02/chat-applicaton/public/javascripts/chat.js
@@ -1,45 +1,49 @@
+
+// 定义一个 Chat 类
 var Chat = function(socket) {
-  this.socket = socket;
+    this.socket = socket;
 };
 
+// 发送消息
 Chat.prototype.sendMessage = function(room, text) {
-  var message = {
-    room: room,
-    text: text
-  };
-  this.socket.emit('message', message);
+    var message = { 
+        room: room,
+        text: text      
+    };
+    this.socket.emit("message", message);
 };
 
+// 更改房间
 Chat.prototype.changeRoom = function(room) {
-  this.socket.emit('join', {
-    newRoom: room
-  });
+    this.socket.emit("join", {
+        newRoom: room
+    });
 };
 
-console.log(11111);
-
+// 处理聊天命令
 Chat.prototype.processCommand = function(command) {
-  var words = command.split(' ');
-  var command = words[0]
-                .substring(1, words[0].length)
-                .toLowerCase();
-  var message = false;
+    var words = command.split(" ");
+    // 从第一个单词开始解析命令
+    var command = words[0].substring(1, words[0].length).toLowerCase();
 
-  switch(command) {
-    case 'join':
-      words.shift();
-      var room = words.join(' ');
-      this.changeRoom(room);
-      break;
-    case 'nick':
-      words.shift();
-      var name = words.join(' ');
-      this.socket.emit('nameAttempt', name);
-      break;
-    default:
-      message = 'Unrecognized command.';
-      break;
-  };
+    var message = false;
 
-  return message;
+    switch (command) {
+        case: "join":
+            words.shift();
+            var room = words.join(" ");
+            // 处理房间的变更
+            this.changeRoom(room);
+            break;
+        case: "nick":
+            words.shift();
+            var name = words.join(" ");
+            // 更换昵称
+            this.socket.emit("nameAttempt", name);
+        default:
+            // 无法识别的命令，则返回错误
+            message = "Unrecognized command";
+            break;
+    }
+    return message;
 };
--- chapter02/chat-applicaton/public/javascripts/chat_ui.js
+++ chapter02/chat-applicaton/public/javascripts/chat_ui.js
@@ -1,79 +1,28 @@
+
+// 对特殊字符进行转义
 function divEscapedContentElement(message) {
-  return $('<div></div>').text(message);
+    return $("<div></div>").text(message);
 }
 
+// 显示系统的消息
 function divSystemContentElement(message) {
-  return $('<div></div>').html('<i>' + message + '</i>');
+    return $("<div></div>").html("<i>" + message + "</i>");
 }
+
+
 
 function processUserInput(chatApp, socket) {
-  var message = $('#send-message').val();
-  var systemMessage;
+    var message = $("#send-message").val();
+    var systemMessage;
 
-  if (message.charAt(0) == '/') {
-    systemMessage = chatApp.processCommand(message);
-    if (systemMessage) {
-      $('#messages').append(divSystemContentElement(systemMessage));
-    }
-  } else {
-    chatApp.sendMessage($('#room').text(), message);
-    $('#messages').append(divEscapedContentElement(message));
-    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
-  }
-
-  $('#send-message').val('');
-}
-
-var socket = io.connect();
-
-$(document).ready(function() {
-  var chatApp = new Chat(socket);
-
-  socket.on('nameResult', function(result) {
-    var message;
-
-    if (result.success) {
-      message = 'You are now known as ' + result.name + '.';
+    // 如果用户输入的是以 / 开头的内容，将其作为聊天命令
+    if (message.charAt(0) == "/") {
+        systemMessage = chatApp.processCommand(message);
+        if (systemMessage) {
+            $("messages").append(divSystemContentElement(systemMessage));
+        }
     } else {
-      message = result.message;
+        // 将非命令广播给其它用户
+        chatApp.sendMessage($("#room").text(), message);
     }
-    $('#messages').append(divSystemContentElement(message));
-  });
-
-  socket.on('joinResult', function(result) {
-    $('#room').text(result.room);
-    $('#messages').append(divSystemContentElement('Room changed.'));
-  });
-
-  socket.on('message', function (message) {
-    var newElement = $('<div></div>').text(message.text);
-    $('#messages').append(newElement);
-  });
-
-  socket.on('rooms', function(rooms) {
-    $('#room-list').empty();
-
-    for(var room in rooms) {
-      room = room.substring(1, room.length);
-      if (room != '') {
-        $('#room-list').append(divEscapedContentElement(room));
-      }
-    }
-
-    $('#room-list div').click(function() {
-      chatApp.processCommand('/join ' + $(this).text());
-      $('#send-message').focus();
-    });
-  });
-
-  setInterval(function() {
-    socket.emit('rooms');
-  }, 1000);
-
-  $('#send-message').focus();
-
-  $('#send-form').submit(function() {
-    processUserInput(chatApp, socket);
-    return false;
-  });
-});
+}
