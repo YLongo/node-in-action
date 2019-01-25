9a0946d36969c91c18dfb471f96ca7b632bc3ffd
Volong <yhlona523@gmail.com>
2019-01-26 00:52:31 +0800

    post and get

---
+++ chapter03/currency.js
@@ -0,0 +1,16 @@
+var canadianDollar = 0.91;
+
+function roundTwoDecimals(amount) {
+  return Math.round(amount * 100) / 100;
+}
+
+// 将 canadianToUs 函数设定在 exports 模块中，所有引入这个模块的代码可以使用它
+exports.canadianToUS = function(canadian) {
+  return roundTwoDecimals(canadian * canadianDollar);
+}
+
+// USToCanadian 设置到 exports 模块中
+exports.USToCanadian = function(us) {
+	return roundTwoDecimals(us / canadianDollar);
+}
+
